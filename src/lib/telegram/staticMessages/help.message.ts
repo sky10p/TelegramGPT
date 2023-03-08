4c9c534c812bc746340354253735b089bc4409f4
Pablo Guijarro <sky10pg@gmail.com>
2023-03-08 08:26:19 +0000

    feat: changed help

--- src/lib/telegram/staticMessages/help.message.ts
+++ src/lib/telegram/staticMessages/help.message.ts
@@ -1,15 +1,21 @@
-export const helpMessage = `🆘 ¡Hola! Soy el bot de TelegramGPT y estoy aquí para ayudarte. Estas son las acciones que puedes realizar conmigo:
+🆘 ¡Hola! Soy el bot de TelegramGPT y estoy aquí para ayudarte. Estas son las acciones que puedes realizar conmigo:
 
 ❓ /help - Si necesitas ayuda adicional, utiliza este comando para obtener información sobre cómo utilizar todas estas acciones.
 
 💬 /chat - ¡Empecemos una conversación! Podemos hablar sobre lo que quieras.
 
+📝 /summarize - Si necesitas un resumen de un texto largo, utiliza este comando para obtener un resumen rápido y conciso.
+
+💯 /improve - ¿Tienes un texto que necesitas mejorar? Utiliza este comando para enviar el texto y obtener una explicación mejorada con correcciones gramaticales.
+
+🔑 /key_points - Si necesitas extraer los puntos más importantes de un texto, utiliza este comando para obtener una lista de los puntos clave.
+
 🖼️ /generation_image - ¡Generemos una imagen! Envíame una descripción o una palabra clave y te enviaré una imagen relacionada.
 
 💰 /usage_day - Consulta el precio por día de mi servicio.
 
-💸 /usage_month - Consulta el precio por mes de mi servicio.
+�� /usage_month - Consulta el precio por mes de mi servicio.
 
 ❌ /cancel - Si en algún momento quieres cancelar la operación actual, utiliza este comando.
 
-Espero que esto te sea de ayuda. ¡No dudes en preguntarme cualquier cosa que necesites! 😀`
+Espero que esto te sea de ayuda. ¡No dudes en preguntarme cualquier cosa que necesites! 😀
